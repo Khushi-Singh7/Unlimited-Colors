69b54006f1e91c99c2601c5728317f3394710fe7
Khushi-Singh7 <sintejpal103@gmail.com>
2024-11-15 18:37:12 +0530

    Add files via upload

---
+++ unlimited colors/project6.js
@@ -0,0 +1,31 @@
+const randomcolor= function(){
+    const hex = "0123456789ABCDEF"
+    let color="#"
+   for(let i=0;i<6;i++) {
+    color += hex[Math.floor(Math.random()*16)]
+
+    }
+    return color;
+}
+let intervalId
+
+    const startchangingcolor=function () {
+        if(!intervalId){
+  intervalId=setInterval(changebgcolor,1000)
+        }
+        function changebgcolor(){
+            document.body.style.backgroundColor= randomcolor()
+    }
+    }
+
+const stopchangingcolor=function(){
+    clearInterval(intervalId)
+    intervalId=null
+}
+
+
+//console.log(randomcolor())
+document.querySelector("#start").addEventListener('click',startchangingcolor)
+
+console.log(randomcolor())
+document.querySelector("#stop").addEventListener('click',stopchangingcolor)
